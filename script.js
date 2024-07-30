6a024a74f7cdcf04671e4fb62000e1586d759bfd
angeldoudou <angelrwaka.doudouu@gmail.com>
2024-07-30 04:44:21 -0700

    my first code

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('captcha-checkbox').addEventListener('change', function() {
+    if (this.checked) {
+        alert("Checkbox is checked.");
+    } else {
+        alert("Checkbox is unchecked.");
+    }
+});
